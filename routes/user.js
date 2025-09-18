91998c841e6259f139ce71999f0a8a724110a6eb
heebraim <heebraim>
2025-09-18 09:50:56 -0500

    afternewtestimonies

--- routes/user.js
+++ routes/user.js
@@ -13,6 +13,9 @@ router.get("/", async (req, res) => {
 router.get("/ilorin", async (req, res) => {
   res.render("fpm-ilorin");
 });
+router.get("/join-service route", async (req, res) => {
+  res.render("https://us05web.zoom.us/j/84087359205?pwd=0dmvED");
+});
 
 // this are the routes for the other branches
 
